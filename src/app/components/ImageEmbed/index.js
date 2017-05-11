3e8b81de4efdcf75dc5773aaf286c45e3c567799
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-05-11 11:00:18 +1000

    Add links to embedded images.

--- src/app/components/ImageEmbed/index.js
+++ src/app/components/ImageEmbed/index.js
@@ -1,5 +1,6 @@
 // External
 const html = require('bel');
+const url2cmid = require('util-url2cmid');
 
 // Ours
 const {before, detach, select} = require('../../../utils');
@@ -22,13 +23,19 @@ function transformEl(el, preserveOriginalRatio) {
   const imgEl = select('img', el);
 
   if (imgEl) {
+    const src = imgEl.src;
+    const alt = imgEl.getAttribute('alt');
+    const id = url2cmid(src);
+    const linkUrl = `/news/${id}`;
+
     const imageEmbedEl = ImageEmbed({
       pictureEl: Picture({
-        src: imgEl.src,
-        alt: imgEl.getAttribute('alt'),
+        src,
+        alt,
         smRatio: '3x4',
         mdRatio: '4x3',
-        preserveOriginalRatio
+        preserveOriginalRatio,
+        linkUrl
       }),
       captionEl: Caption.createFromEl(el)
     });
