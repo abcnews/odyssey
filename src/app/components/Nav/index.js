839d91f598ab98908af76633b1a41a05126a5b25
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-06-09 17:40:57 +1000

    Improved nav bar show/hide stuff.

--- src/app/components/Nav/index.js
+++ src/app/components/Nav/index.js
@@ -24,16 +24,19 @@ module.exports = function Nav({homeHref = '/news/', shareLinks}) {
   subscribe(function _checkIfNavBarPropertiesShouldBeUpdated() {
     const rect = navEl.getBoundingClientRect();
     const top = rect.top;
-    const diff = top - state.top;
-    const isFixed = top <= 0;
+    const diff = state.top - top;
+    const isAbove = top <= 0;
 
-    if (isFixed !== state.isFixed || !isFixed || Math.abs(diff) > 10) {
+    if (
+      isAbove !== state.isAbove ||
+      isAbove && Math.abs(diff) > 10
+    ) {
       enqueue(function _updateNavBarProperties() {
-        navBarEl.classList[isFixed ? 'add' : 'remove']('is-fixed');
-        navBarEl.classList[!isFixed || diff > 0 ? 'add' : 'remove']('is-peeking');
+        navEl.classList[isAbove ? 'add' : 'remove']('is-above');
+        navBarEl.classList[isAbove && diff > 0 ? 'add' : 'remove']('is-hiding');
       });
 
-      state = {top, diff, isFixed};
+      state = {top, isAbove};
     }
   });
 
