4ab67a807d69d481275a7154058f2231ce9a2b34
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-09-13 14:51:08 +1000

    Completed Comments integration.

--- src/app/components/Comments/index.js
+++ src/app/components/Comments/index.js
@@ -2,12 +2,17 @@
 const html = require('bel');
 
 // Ours
+const {append} = require('../../utils/dom');
 require('./index.scss');
 
 function Comments() {
   const livefyreRootEl = html`
     <div class="Comments-livefyreRoot u-layout"></div>
   `;
+
+  if (ABC.News.Mobile) {
+    append(livefyreRootEl, html`<h2>Have your say</h2>`);
+  }
 
   if (ABC.News.initLivefyre) {
     ABC.News.initLivefyre(() => {
