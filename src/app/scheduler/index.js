cab2355d89b88266b3be9f706ddf241a3b94f872
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-10-03 16:55:12 +1000

    Fixed arguments in invalidateClient

--- src/app/scheduler/index.js
+++ src/app/scheduler/index.js
@@ -10,7 +10,7 @@ const BUDGETED_MILLISECONDS_PER_FRAME = 12;
 
 const subscribers = [];
 const queue = [];
-let client = null
+let client = null;
 let hasStarted;
 
 function flush() {
@@ -81,7 +81,7 @@ function onResize(event) {
 }
 
 function invalidateClient() {
-  enqueue(onResize.bind(null, null, true));
+  enqueue(onResize.bind(null, true));
 }
 
 function start() {
