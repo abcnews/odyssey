70e051be2e32088a9cf5fad290b380a11d855991
Nathan Hoad <nathan@nathanhoad.net>
2018-08-29 11:03:07 +1000

    Odyssey loading can be overridden with query param

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-// Polyfills
-require('./polyfills');
-require('objectFitPolyfill');
+const { IS_PREVIEW, QUERY } = require('./constants');
 
-// Global styles
-require('./fonts.scss');
-require('./keyframes.scss');
-require('./app/components/utilities/index.scss');
+let shouldLoadNow = true;
 
-// App
-require('./app')();
+if (IS_PREVIEW && QUERY['odyssey']) {
+  const odyssey = QUERY['odyssey'].split('-');
+  const scriptUrl = `//${odyssey[0]}.aus.aunty.abc.net.au:${odyssey[1] || 8000}/index.js`;
+
+  if (!document.querySelector(`script[src="${scriptUrl}"]`)) {
+    shouldLoadNow = false;
+
+    const script = document.createElement('script');
+    script.src = scriptUrl;
+    document.querySelector('head').appendChild(script);
+  }
+}
+
+if (shouldLoadNow) {
+  // Polyfills
+  require('./polyfills');
+  require('objectFitPolyfill');
+
+  // Global styles
+  require('./fonts.scss');
+  require('./keyframes.scss');
+  require('./app/components/utilities/index.scss');
+
+  // App
+  require('./app')();
+}
