4e1ff4291f99e92d64f407203bb615452c579c52
Colin Gourlay <gourlay.colin@abc.net.au>
2020-09-16 13:31:13 +1000

    Polyfill Array.prototype.fill. Bundle all polyfills together again to get size reductions from deduping

--- src/app/async-components/polyfills.js
+++
@@ -1,4 +0,0 @@
-// ECMAScript
-require('core-js/stable/object/entries');
-require('core-js/stable/string/starts-with');
-require('core-js/stable/symbol/iterator');
--- src/polyfills/index.js
+++ src/polyfills/index.js
@@ -1,6 +1,10 @@
 // ECMAScript
+require('core-js/stable/array/fill');
 require('core-js/stable/dom-collections/iterator');
+require('core-js/stable/object/entries');
+require('core-js/stable/string/starts-with');
 require('core-js/stable/symbol');
+require('core-js/stable/symbol/iterator');
 // Browser APIs
 require('custom-event-polyfill');
 require('ric');
