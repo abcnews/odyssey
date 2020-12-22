dc7014bc061be6ca20cee216042e44d219fb8c82
Colin Gourlay <gourlay.colin@abc.net.au>
2020-12-22 15:48:40 +1000

    Indicate to P1 that we're overriding the article and implementing audio/video players, when we execute as Associated JS

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ require('./fonts.scss');
 require('./keyframes.scss');
 require('./app/components/utilities/index.scss');
 require('./polyfills');
-require('@abcnews/env-utils')
-  .requestDOMPermit('body')
-  .then(require('./app'));
+const { GENERATIONS, getGeneration, requestDOMPermit } = require('@abcnews/env-utils');
+
+// When this runs as Associated JS, rather than init-interactive,
+// we still need to let Phase 1 know (somehow) that we intend to
+// takeover the article and implement our own audio/video players
+if (getGeneration() === GENERATIONS.P1) {
+  const implementsEl = document.createElement('meta');
+
+  implementsEl.className = 'init-interactive';
+  implementsEl.setAttribute('data-implements', 'article,audio,video');
+  document.head.appendChild(implementsEl);
+}
+
+// Once we know we're allowed to modify the DOM, run the app
+requestDOMPermit('body').then(require('./app'));
