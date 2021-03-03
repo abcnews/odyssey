0cdb4373e2511110cf73c9bc7db3f33c9d2208ce
Colin Gourlay <gourlay.colin@abc.net.au>
2021-03-03 17:16:16 +1000

    Use correct Terminus version for environment (preview of CM5 content still comes from V1)

--- src/app/utils/content.js
+++ src/app/utils/content.js
@@ -1,9 +1,11 @@
+const { TIERS, getTier } = require('@abcnews/env-utils');
 const { fetchOne } = require('@abcnews/terminus-fetch');
 
 const API_KEY = '***REMOVED***';
+const VERSION = getTier() === TIERS.PREVIEW ? 'v1' : 'v2';
 
 module.exports.terminusFetch = (_options, done) => {
   const options = typeof _options === 'object' ? _options : { id: _options };
 
-  return fetchOne({ apikey: API_KEY, ...options }, done);
+  return fetchOne({ apikey: API_KEY, version: VERSION, ...options }, done);
 };
