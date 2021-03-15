5c65b6d8f14f271f0e87f1512f063fc92c97bc70
Simon Elvery <simon@elvery.net>
2021-03-16 09:12:36 +1000

    Switch to terminus v2 for preview

--- src/app/utils/content.js
+++ src/app/utils/content.js
@@ -1,20 +1,20 @@
-const { GENERATIONS, TIERS, getGeneration, getTier } = require('@abcnews/env-utils');
+const { GENERATIONS, getGeneration } = require('@abcnews/env-utils');
 const { fetchOne } = require('@abcnews/terminus-fetch');
 
 const API_KEY = '***REMOVED***';
-const VERSION = getGeneration() !== GENERATIONS.PL || getTier() === TIERS.PREVIEW ? 'v1' : 'v2';
+const VERSION = getGeneration() !== GENERATIONS.PL ? 'v1' : 'v2';
 
 // The Terminus version we use depends on the environment, so that we can match
 // the version to the one which rendered the page (or use v1 for CAPI sources).
 // Whether that is live or preview Terminus is handled by @abcnews/terminus-fetch
 //
 // - Use v1 for Phase 1 / Phase 2 generation apps (live or preview tiers)
-// - Use v1* for PL generation apps on the preview tier
-// - Use v2 for PL generation apps on the live tier
+// - Use v2 for PL generation apps unless there is a URL query params override
 //
-// *: @abcnews/terminus-fetch will allow the endpoint (including version) to
-// be overridden by the terminusBaseURL URL param, which is useful because CM10
-// uses it to render its own (PL generation) preview windows with v2.
+// The assumptions about which Terminus version to use for PL rendered pages are
+// now in line with the assumptions made in @abcnews/terminus-fetch. That is, use
+// v2 unless the terminusBaseURL query param exists, which specifies a version to
+// use.
 
 module.exports.terminusFetch = (_options, done) => {
   const options = typeof _options === 'object' ? _options : { id: _options };
