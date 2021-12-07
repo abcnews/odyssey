255fe23e1ed661a9663821c68769558b3b0fb594
Colin Gourlay <colin@colin-gourlay.com>
2021-12-07 11:40:08 +1000

    Destructure props

--- src/app/async-components/Interactive/index.js
+++ src/app/async-components/Interactive/index.js
@@ -18,12 +18,13 @@ const LOADER_NAME = 'oembed';
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 module.exports = props => {
+  const { alignment, embedURL, providerType } = props;
   const ref = useRef(null);
   const { data, error } = useSWRImmutable(
     `${LOADERS_ENDPOINT}${LOADER_NAME}?${new URLSearchParams({
-      alignment: props.alignment,
-      providerType: props.providerType,
-      url: props.embedURL
+      alignment,
+      providerType,
+      url: embedURL
     }).toString()}`,
     fetcher
   );
@@ -37,7 +38,7 @@ module.exports = props => {
     ref.current.appendChild(document.createRange().createContextualFragment(data.html));
 
     // Additional steps to take, in case 3rd party libraries had already been loaded and executed
-    switch (props.providerType) {
+    switch (providerType) {
       case 'facebook':
       case 'facebookVideo':
         if (window.FB) {
@@ -61,7 +62,7 @@ module.exports = props => {
 
   let placeholder = null;
 
-  if (!SUPPORTED_PROVIDER_TYPES.includes(props.providerType)) {
+  if (!SUPPORTED_PROVIDER_TYPES.includes(providerType)) {
     placeholder = <HiddenErrorMessage message="Unsupported provider type" {...props} />;
   } else if (error) {
     placeholder = <HiddenErrorMessage message={`Error loading data: "${String(error)}"`} {...props} />;
