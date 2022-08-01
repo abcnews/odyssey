3a7a5688df76bb757df9e829d20e1ef3670eb11e
Colin Gourlay <colin@colin-gourlay.com>
2022-08-01 10:11:55 +1000

    Remove associated JS takeover hacks for P1's news-core

--- src/index.js
+++ src/index.js
@@ -21,17 +21,6 @@ proxy('odyssey').then(() => {
     return;
   }
 
-  // When this runs as Associated JS, rather than init-interactive,
-  // we still need to let Phase 1 know (somehow) that we intend to
-  // takeover the article and implement our own audio/video players
-  if (getGeneration() === GENERATIONS.P1) {
-    const implementsEl = document.createElement('meta');
-
-    implementsEl.className = 'init-interactive';
-    implementsEl.setAttribute('data-implements', 'article,audio,image,video');
-    document.head.appendChild(implementsEl);
-  }
-
   // Once we've got:
   // 1. the article's terminus document, and
   // 2. permission to modify the DOM
