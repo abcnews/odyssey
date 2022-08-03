b718979b0d6d3e32037da812df309312ece9323f
Colin Gourlay <colin@colin-gourlay.com>
2022-08-03 16:34:04 +1000

    Change aunty project type from react to basic and simplify config. Remove unused dependencies.

--- aunty.config.js
+++ aunty.config.js
@@ -1,44 +1,9 @@
-const path = require('path');
-const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
-
 module.exports = {
-  type: 'react',
+  type: 'basic',
   build: {
     useCSSModules: false
   },
   serve: {
     hot: false
-  },
-  webpack: config => {
-    // Help us identify things we dan try to dedupe imports
-    config.plugins.push(new DuplicatePackageCheckerPlugin({ showHelp: false }));
-
-    // Use resolve.alias to dedupe
-    config.resolve = config.resolve || {};
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      react: path.resolve(__dirname, 'node_modules/react'),
-      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-      'react-is': path.resolve(__dirname, 'node_modules/downshift/node_modules/react-is')
-    };
-
-    // Stop `import()`-ed chunks from being split into `[name].js` and `vendors~[name].js`
-    config.optimization = {
-      ...(config.optimization || {}),
-      splitChunks: {
-        cacheGroups: {
-          defaultVendors: false
-        }
-      }
-    };
-
-    // Allow larger entrypoint & assets sizes than the default (250000)
-    config.performance = {
-      ...(config.performance || {}),
-      maxAssetSize: 500000,
-      maxEntrypointSize: 500000
-    };
-
-    return config;
   }
 };
