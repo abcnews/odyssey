54a0ad466798b04fe58eb33d051b097bece7a828
Colin Gourlay <colin@colin-gourlay.com>
2022-08-03 16:33:13 +1000

    Remove async-components entirely

--- src/app/async-components/loader.js
+++
@@ -1,8 +0,0 @@
-export const AsyncComponent = (componentName, props) => {
-  const el = document.createElement('div');
-
-  el.setAttribute('data-async-component', componentName);
-  import(/* webpackChunkName: "async-components" */ '.').then(({ render }) => render(componentName, props, el));
-
-  return el;
-};
