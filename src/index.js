6c8a2a27d69ca3d7ecbbbb8352f702cd16665260
Colin Gourlay <colin@colin-gourlay.com>
2022-08-05 10:03:00 +1000

    Add promise-resolution-based debug function. Break up initial async tasks so we can log them as they happen. Add bail-out reason logging

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { GENERATIONS, getGeneration, requestDOMPermit } from '@abcnews/env-utils
 import { url2cmid } from '@abcnews/url2cmid';
 import app from './app';
 import { terminusFetch } from './app/utils/content';
-import { debug } from './app/utils/logging';
+import { debug, debugWhen } from './app/utils/logging';
 
 // Provide a hint as early as possible that the Odyssey format will be driving
 // this story, so that other interactives can opt to wait for Odyssey to load
@@ -17,20 +17,21 @@ window.__IS_ODYSSEY_FORMAT__ = true;
 proxy('odyssey').then(() => {
   // Don't run on IE or old-Edge, which we no longer support
   if (/* IE <= 9 */ (document.all && !window.atob) || /* IE >= 10 */ window.navigator.msPointerEnabled) {
-    return;
+    return debug('Trident-based browsers are not supported');
   }
 
   // Don't run on non-PL website generations
   if (getGeneration() !== GENERATIONS.PL) {
-    return;
+    return debug('Non-Presentation Layer ABC websites are not supported');
   }
 
   // Once we've got:
-  // 1. the article's terminus document, and
-  // 2. permission to modify the DOM
-  // ...we can run the run the app, using the terminus document to initialise the metadata that's used everywhere
-  Promise.all([terminusFetch(url2cmid(window.location.href)), requestDOMPermit('body')]).then(([terminusDocument]) => {
-    debug('Fetched Terminus article document and obtained DOM permit for "body"');
-    app(terminusDocument);
-  });
+  //   1. the article's terminus document, and
+  const terminusFetchTask = terminusFetch(url2cmid(window.location.href));
+  debugWhen(terminusFetchTask, 'Fetched Terminus article document');
+  //   2. permission to modify the DOM
+  const requestDOMPermitTask = requestDOMPermit('body');
+  debugWhen(requestDOMPermitTask, 'Obtained DOM permit for "body"');
+  // ...we can run the app, using the terminus document to initialise metadata
+  Promise.all([terminusFetchTask, requestDOMPermitTask]).then(([terminusDocument]) => app(terminusDocument));
 });
