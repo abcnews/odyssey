00085c72d306dabebccd98c6557d9fb6fa495865
Colin Gourlay <colin@colin-gourlay.com>
2022-08-12 11:45:05 +1000

    Cache terminus fetch requests by key so documents are only fetched once.

--- src/app/utils/content.js
+++ src/app/utils/content.js
@@ -2,8 +2,15 @@ import { fetchOne } from '@abcnews/terminus-fetch';
 
 const API_KEY = '***REMOVED***';
 
-export const terminusFetch = (_options, done) => {
-  const options = typeof _options === 'object' ? _options : { id: _options };
+const cache = {};
 
-  return fetchOne({ apikey: API_KEY, ...options }, done);
+export const terminusFetch = _options => {
+  const options = typeof _options === 'object' ? _options : { id: _options };
+  const key = options.id;
+
+  if (!cache[key]) {
+    cache[key] = fetchOne({ apikey: API_KEY, ...options });
+  }
+
+  return cache[key];
 };
