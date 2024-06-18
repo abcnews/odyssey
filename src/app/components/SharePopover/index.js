75778631dd9398dde1999560a43ca9e4058913f4
Julian Fell <hi@jtfell.com>
2024-06-18 16:59:00 +1000

    fix mobile version of share popover

--- src/app/components/SharePopover/index.js
+++ src/app/components/SharePopover/index.js
@@ -5,10 +5,6 @@ import { getMeta } from '../../meta';
 import { subscribe, unsubscribe } from '../../scheduler';
 import styles from './index.lazy.scss';
 
-const DEFAULT_TYPE = 'story';
-const INVITATION_RANGE = 0;
-const UPPERCASE_PATTERN = /[A-Z]/g;
-
 const SharePopover = ({ links, onClose }) => {
   styles.use();
 
@@ -31,21 +27,19 @@ const SharePopover = ({ links, onClose }) => {
 export default SharePopover;
 
 const PATHS = {
-  email:
-    '',
+  email: '',
   facebook:
     'M9.668 21h3.335v-8.996h2.502L16 9.194h-2.997V7.157c0-.759.309-1.346 1.26-1.346h1.494V3h-2.088c-3.24.013-4 1.99-3.995 3.937l-.006 2.257H8v2.81h1.668V21Z',
   native: '',
-  linkedin: 'M16.529 8.677c3.774 0 4.471 2.49 4.471 5.73V21h-3.727v-5.847c0-1.395-.027-3.188-1.939-3.188-1.94 0-2.236 1.519-2.236 3.086V21H9.372V8.977h3.574v1.641h.052c.498-.944 1.716-1.94 3.53-1.94Zm-9.498.3V21H3.296V8.977h3.735ZM5.164 3a2.165 2.165 0 0 1 0 4.332A2.164 2.164 0 0 1 3 5.165C3 3.97 3.967 3 5.164 3Z',
+  linkedin:
+    'M16.529 8.677c3.774 0 4.471 2.49 4.471 5.73V21h-3.727v-5.847c0-1.395-.027-3.188-1.939-3.188-1.94 0-2.236 1.519-2.236 3.086V21H9.372V8.977h3.574v1.641h.052c.498-.944 1.716-1.94 3.53-1.94Zm-9.498.3V21H3.296V8.977h3.735ZM5.164 3a2.165 2.165 0 0 1 0 4.332A2.164 2.164 0 0 1 3 5.165C3 3.97 3.967 3 5.164 3Z',
   twitter:
     'M13.317 10.775 19.146 4h-1.381l-5.061 5.883L8.662 4H4l6.112 8.896L4 20h1.381l5.344-6.212L14.994 20h4.662l-6.339-9.225ZM5.88 5.04H8l9.765 13.968h-2.121L5.879 5.04Z'
 };
 
 const LABELS = {
-  email:
-    '',
-  facebook:
-    'Facebook',
+  email: '',
+  facebook: 'Facebook',
   native: '',
   linkedin: '',
   twitter: 'X (formerly Twitter)'
