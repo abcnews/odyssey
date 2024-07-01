904c4a42a13312c756c66c7e3b16dcb7d005dcb6
Simon Elvery <simon@elvery.net>
2024-07-02 09:22:31 +1000

    Revert dropcap par length threshold

--- src/app/components/UDropcap/index.js
+++ src/app/components/UDropcap/index.js
@@ -3,10 +3,9 @@
 /**
  * Only apply the dropcap class to text elements where it won't look silly.
  * @param {Element|null} el
- * @param {boolean} isFuture
  */
-export const conditionallyApply = (el, isFuture = false) => {
-  if (el !== null && el.tagName === 'P' && (el.textContent?.length || 0) > (isFuture ? 140 : 80)) {
+export const conditionallyApply = el => {
+  if (el !== null && el.tagName === 'P' && (el.textContent?.length || 0) > 80) {
     el.classList.add('u-dropcap');
   }
 };
