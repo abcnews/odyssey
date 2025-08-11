81eafee3bec1087a27b01f4cc8069debd0bea95e
Simon Elvery <simon@elvery.net>
2025-08-11 17:04:53 +1000

    Fix visual tests

--- playwright.config.js
+++ playwright.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
   // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  timeout: 60000,
   expect: {
     toHaveScreenshot: { maxDiffPixels: 500, maxDiffPixelRatio: 0.02 }
   },
